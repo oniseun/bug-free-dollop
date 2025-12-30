22877e4ae9b3e5ebb994f0dda00863d2c091d089
oniseun <seunoni34@gmail.com>
2025-12-30 10:29:25 +0000

    refactor(product): pull user relation through products

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -4,12 +4,15 @@ import { Product } from './entities/product.entity';
 import { ProductController } from './controllers/product.controller';
 import { ProductService } from './services/product.service';
 import { ProductRepository } from './repositories/product.repository';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    UsersModule,
+  ],
   controllers: [ProductController],
   providers: [ProductService, ProductRepository],
   exports: [ProductService],
 })
 export class ProductsModule {}
-
